ceb8a1e78fa98be1a9a16cb70bd49ec3cd073c51
unknown <tahseen9832@gmail.com>
2026-09-09 16:28:35 +0530

    queue1.c

--- queue1.c
+++ queue1.c
@@ -1,39 +1,135 @@
 #include <stdio.h>
-#define MAX=100
-int stack[MAX];
-int top=-1;
-void push(int value){
-    stack[++top]=value;
+#define MAX 5
+int queue[MAX];
+int front = -1;
+int rear = -1;
+void enqueue()
+{
+    int value;
+    printf("Enter value to insert: ");
+    scanf("%d", &value);
+     if ((rear + 1) % MAX == front)
+    {
+        printf("Circular Queue Overflow!\n");
+        return;
+    }
+    if (front == -1)
+    {  front = 0;  rear = 0;    }
+    else
+    {     rear = (rear + 1) % MAX;    }
+
+    queue[rear] = value;
+
+    printf("%d inserted successfully.\n", value);
 }
-int pop(){
-    return stack[top--];
+void dequeue()
+{
+    int value;
+  if (front == -1)
+    {   printf("Circular Queue Underflow!\n");   return;    }
+    value = queue[front];
+    printf("%d deleted successfully.\n", value);
+   if (front == rear)
+    {  front = -1;        rear = -1;    }
+    else
+    {        
+        front = (front + 1) % MAX;
+    }
 }
-int main(){
-    char postfix[MAX];
-    int i,a,b,result;
-    printf("Enter the postfix expression:");
-    scanf("%s",postfix);
-    for(i=0;postfix[i]!='\0'; i++){
-        if(isdigit(postfix[i])){
-            push(postfix[i]-'0');
+void peek()
+{
+    if (front == -1)
+    {
+        printf("Queue is empty.\n");
+        return;
+    }
+
+    printf("Front element = %d\n", queue[front]);
+}
+void display()
+{
+    int i;
+    if (front == -1)
+    {
+        printf("Queue is empty.\n");
+        return;
+    }
+    printf("Circular Queue: ");
+    i = front;
+    while (1)
+    {
+        printf("%d ", queue[i]);
+       if (i == rear)
+            break;
+        i = (i + 1) % MAX;
+    }
+    printf("\n");
+}
+void count()
+{
+    int i;
+    int count = 0;
+
+    if (front == -1)
+    {
+        printf("Number of elements = 0\n");
+        return;
+    }
+    i = front;
+    while (1)
+    {
+        count++;
+
+        if (i == rear)
+            break;
+
+        i = (i + 1) % MAX;
+    }
+    printf("Number of elements = %d\n", count);
+}
+
+int main()
+{
+    int choice;
+
+    while (1)
+    {
+        printf("\n");
+        printf("=================================\n");
+        printf("       CIRCULAR QUEUE MENU\n");
+        printf("=================================\n");
+        printf("1. Enqueue\n");
+        printf("2. Dequeue\n");
+        printf("3. Peek\n");
+        printf("4. Display\n");
+        printf("5. Count\n");
+        printf("6. Exit\n");
+        printf("=================================\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+            case 1:
+                enqueue();
+                break;
+            case 2:
+                dequeue();
+                break;
+            case 3:
+                peek();
+                break;
+            case 4:
+                display();
+                break;
+            case 5:
+                count();
+                break;
+            case 6:
+                printf("Program terminated.\n");
+                return 0;
+            default:
+                printf("Invalid choice! Please try again.\n");
         }
-        else{
-            b=pop();
-            a=pop();
-            switch(postfix[i]){
-                case '+':
-                    result=a+b;
-                    break;
-                case '-':
-                    result=a-b;
-                    break;
-                case '*':
-                    result=a*b;
-                    break;
-                case '/':
-                    result=a/b;
-                    break;
-            }
-            push(result);
-        }
+    }
+    return 0;
 }
